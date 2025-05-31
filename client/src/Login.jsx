9146e457b226232bcbd8798ac58a9c5a5a29d209
KBhumee17 <bhumee.git@gmail.com>
2025-05-31 14:48:50 +0530

    Initial commit - Login/Register project with client and server

---
+++ client/src/Login.jsx
@@ -0,0 +1,71 @@
+import React from "react"; 
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/login", { email, password })
+        .then(result => {
+            console.log(result)
+            if (result.data === "Success") {
+                navigate('/home');  // absolute path
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+    return (
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+            <div className="bg-white p-4 rounded w-100" style={{ maxWidth: "400px" }}>
+                <h2 className="mb-4 text-center">Login</h2>
+                <form onSubmit={handleSubmit}>
+                    
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">
+                            <strong>Email</strong>
+                            </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter Email"
+                            autoComplete="off"
+                            className="form-control rounded-0"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">
+                            <strong>Password</strong>
+                            </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Enter Password"
+                            className="form-control rounded-0"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 mb-2">
+                        Login
+                    </button>
+                    </form>
+                    <p>Already Have an Account</p>
+                    <Link to="/register" className="btn btn-default border w-100 bg-light rounded-0 ">
+                        Sign Up
+                    </Link>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Login;
